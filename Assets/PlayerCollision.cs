7d23d4ad0572d36104d130925382ead95b35efec
luca <lucacm@al.insper.edu.br>
2022-11-25 09:44:09 -0300

    trabalhando mais no jogo

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
